1d87af45617d3c760dfe457e35edf02bce100c23
Jacco Bruin <94831668+Covid-taskforce-veneficus@users.noreply.github.com>
2023-05-11 18:53:41 +0200

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -67,7 +67,7 @@ def main():
     prediction = getpred(input_json)
 
     st.write(
-        "**The predicted number of orders:**", st.write(math.floor(prediction), style={'font-size': '50px'})
+        "**The predicted number of orders:**", st.write(math.floor(prediction), style={'font-size': '100px'})
     )  # ik rond de orders naar boven af
 
 
